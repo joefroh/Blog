2406c582ce6cba8639165862b04a97fee0cefd0d
joefroh <jmafrohlinger@outlook.com>
2014-10-14 23:17:31 -0700

    Adding Node Class

---
+++ DataStructuresAndAlgorithms/DataStructuresAndAlgorithms/Node.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgorithms
+{
+    public class Node<T>
+    {
+        private Node<T> _previous ;
+        private Node<T> _next;
+        private T _data;
+
+        public Node(T data)
+        {
+            _data = data;
+        }
+
+        public Node<T> Next
+        {
+            get { return _next; }
+            set { _next = value; }
+        }
+
+        public Node<T> Previous
+        {
+            get { return _previous; }
+            set { _previous = value; }
+        }
+
+        public T Data
+        {
+            get { return _data; }
+            set { _data = value; }
+        }
+
+    }
+}
