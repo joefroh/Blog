7811d6e884e0fb9484dce1be3fdc286592723cc9
joefroh <jmafrohlinger@outlook.com>
2014-10-24 23:41:16 -0700

    partially implemented class, with some tests lighting up. Red green ftw

--- DataStructuresAndAlgorithms/DataStructuresAndAlgorithms/SinglyLinkedList.cs
+++ DataStructuresAndAlgorithms/DataStructuresAndAlgorithms/SinglyLinkedList.cs
@@ -1,17 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataStructuresAndAlgorithms
 {
     public class SinglyLinkedList<T> : ILinkedList<T>
     {
+        private Node<T> _currentNode;
+        private Node<T> _headNode;
+        private Node<T> _tailNode;
+
+
+        public SinglyLinkedList()
+        {
+            Length = 0;
+            _headNode = null;
+            _tailNode = null;
+            _currentNode = null;
+        }
+
         public int Length { get; private set; }
+
         public void Add(T value)
         {
-            throw new NotImplementedException();
+            if (Length == 0) //nothing in the list.
+            {
+                _headNode = new Node<T>(value);
+                _tailNode = _headNode;
+                Length++;
+            }
+            else if (Length == 1) //single item in list, head == tail
+            {
+                _tailNode.Next = new Node<T>(value);
+                _headNode.Next = _tailNode.Next;
+                _tailNode = _tailNode.Next;
+                Length++;
+            }
+            else
+            {
+                _tailNode.Next = new Node<T>(value);
+                _tailNode = _tailNode.Next;
+                Length++;
+            }
         }
 
         public bool Remove(T value)
@@ -26,12 +54,30 @@ namespace DataStructuresAndAlgorithms
 
         public int Find(T value)
         {
-            throw new NotImplementedException();
+            int index = 0;
+            _currentNode = _headNode;
+
+            while (_currentNode != null)
+            {
+                if (_currentNode.Data.Equals(value))
+                {
+                    return index;
+                }
+                _currentNode = _currentNode.Next;
+                index++;
+            }
+            return -1;
         }
 
         public T GetValueAtIndex(int index)
         {
-            throw new NotImplementedException();
+            _currentNode = _headNode;
+
+            for (int i = 0; i < index; i++)
+            {
+                _currentNode = _currentNode.Next;
+            }
+            return _currentNode.Data;
         }
     }
 }
